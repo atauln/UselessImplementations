b84d5e2cecfb7b08ad7328eb9f2257fd7a82d904
BuildTools <unconfigured@null.spigotmc.org>
2020-10-14 13:27:38 -0400

    Added new PlayerJoinEvent listener, gives iron kit

--- src/com/atom/uselessImplementations/ProjectListener.java
+++ src/com/atom/uselessImplementations/ProjectListener.java
@@ -2,6 +2,7 @@ package com.atom.uselessImplementations;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.AnimalTamer;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -12,6 +13,24 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class ProjectListener implements Listener {
+	
+	@EventHandler
+	public void PlayerJoinEvent(Player playerJoined, String joinMessage) {
+		if (!playerJoined.hasPlayedBefore()) {
+			ItemStack ironPickaxe = new ItemStack(Material.IRON_PICKAXE, 1);
+			ItemStack ironSword = new ItemStack(Material.IRON_SWORD, 1);
+			ItemStack ironAxe = new ItemStack(Material.IRON_AXE, 1);
+			ItemStack ironShovel = new ItemStack(Material.IRON_SHOVEL, 1);
+			ItemStack baseFood = new ItemStack(Material.COOKED_BEEF, 16);
+			ItemStack ironHelm = new ItemStack(Material.IRON_HELMET);
+			ItemStack ironChest = new ItemStack(Material.IRON_CHESTPLATE);
+			ItemStack ironLeggings = new ItemStack(Material.IRON_LEGGINGS);
+			ItemStack ironBoots = new ItemStack(Material.IRON_BOOTS);
+			
+			playerJoined.getInventory().addItem(ironPickaxe, ironSword, ironAxe, ironShovel, baseFood, ironHelm, ironLeggings, ironBoots);
+		}
+	}
+	
 	@EventHandler
 	public void EntityDamageByEntityEvent(Entity damager, Entity damagee, EntityDamageEvent.DamageCause cause, double damage) {
 		damager.sendMessage(ChatColor.GRAY + "You hurt " + damagee.getName() + "! You dealt " + damage + " damage!");
