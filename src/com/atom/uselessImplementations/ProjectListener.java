84b65d4f110d67064b704bfb5c568e49a186c438
BuildTools <unconfigured@null.spigotmc.org>
2020-10-14 12:51:48 -0400

    Added tame event listener

--- src/com/atom/uselessImplementations/ProjectListener.java
+++ src/com/atom/uselessImplementations/ProjectListener.java
@@ -1,7 +1,10 @@
 package com.atom.uselessImplementations;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.AnimalTamer;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -11,5 +14,10 @@ public class ProjectListener implements Listener {
 	public void EntityDamageByEntityEvent(Entity damager, Entity damagee, EntityDamageEvent.DamageCause cause, double damage) {
 		damager.sendMessage(ChatColor.GRAY + "You hurt " + damagee.getName() + "! You dealt " + damage + " damage!");
 		damagee.sendMessage(ChatColor.DARK_RED + "You were hurt by " + damager.getName() + "! You took " + damage + " damage!");
+	}
+	
+	@EventHandler
+	public void EntityTameEvent(LivingEntity tamed, AnimalTamer owner) {
+		Bukkit.broadcastMessage(owner.getName() + " just tamed " + tamed.getName() + "!");
 	}
 }
